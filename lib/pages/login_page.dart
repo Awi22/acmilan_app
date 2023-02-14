e2b9862fd9c93135eaacf4495739ed865f800351
Awi <102220981+Awi22@users.noreply.github.com>
2023-02-14 20:36:01 +0800

    Add login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(
+        children: [
+          //logo
+          Image.asset('images/acm_logo.png'),
+
+          //welcome
+
+          //username
+
+          //password
+
+          //forgot password
+
+          //sign in button
+
+          //or continue with
+
+          //google + apple sign button
+
+          //not a member? register now
+        ],
+      ),
+    );
+  }
+}
