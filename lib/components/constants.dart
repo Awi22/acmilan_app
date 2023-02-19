4a9e93e49eab899424063d1a2ffc94b22facfbd8
Awi <102220981+Awi22@users.noreply.github.com>
2023-02-20 00:05:34 +0800

    Update constant

--- lib/components/constants.dart
+++ lib/components/constants.dart
@@ -1,32 +1,57 @@
+import 'package:acmilan_app/pages/login_page.dart';
+import 'package:acmilan_app/pages/settings.dart';
 import 'package:flutter/material.dart';
 
 var myDefaultBackground = Colors.grey[300];
 
 var myAppBar = AppBar(
+  title: Image.asset(
+    'images/logo.png',
+    height: 48,
+    width: 48,
+  ),
+  centerTitle: true,
   backgroundColor: Colors.grey[900],
 );
 
-var myDrawer = Drawer(
-  backgroundColor: Colors.grey[300],
-  child: Column(
-    children: const [
-      DrawerHeader(child: Icon(Icons.sports_soccer)),
-      ListTile(
-        leading: Icon(Icons.home),
-        title: Text('D A S H B O A R D'),
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.grey[300],
+      child: Column(
+        children: [
+          DrawerHeader(child: Icon(Icons.sports_soccer)),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('D A S H B O A R D'),
+          ),
+          ListTile(
+            leading: Icon(Icons.chat),
+            title: Text('M E S S A G E'),
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('S E T T I N G S'),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return Settings();
+              }));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('L O G O U T'),
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return LoginPage();
+              }));
+            },
+          ),
+        ],
       ),
-      ListTile(
-        leading: Icon(Icons.chat),
-        title: Text('M E S S A G E'),
-      ),
-      ListTile(
-        leading: Icon(Icons.settings),
-        title: Text('S E T T I N G S'),
-      ),
-      ListTile(
-        leading: Icon(Icons.logout),
-        title: Text('L O G O U T'),
-      ),
-    ],
-  ),
-);
+    );
+  }
+}
